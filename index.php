1e73f5a1b2ef7d83bc1044c06a6de5d0ddc42d8a
Adan Felipe Medeiros <adan.grg@gmail.com>
2015-09-15 08:47:51 -0500

    Add starter application package

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Starter Application</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+	<table>
+		<tr>
+			<td style='width: 30%;'><img class = 'newappIcon' src='images/newapp-icon.png'>
+			</td>
+			<td>
+				<h1 id = "message"><?php echo "Hello world!"; ?>
+</h1>
+				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>. Get started by reading our <a
+				href="https://www.ng.bluemix.net/docs/#starters/php/index.html#php">documentation</a>
+				or use the Start Coding guide under your app in your dashboard.
+			</td>
+		</tr>
+	</table>
+</body>
+</html>
