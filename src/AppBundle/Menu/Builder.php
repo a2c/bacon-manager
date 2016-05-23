8e6883e1cc57b0a3653ab7f9cd1e8975dbbaed9b
Adan Felipe Medeiros <adan@redsunlight.com.br>
2016-05-23 10:32:10 -0300

    Ajustes no menu padrao do projeto

--- src/AppBundle/Menu/Builder.php
+++ src/AppBundle/Menu/Builder.php
@@ -25,18 +25,37 @@ class Builder implements ContainerAwareInterface
     public function addMenu(FactoryInterface $factory, array $options)
     {
         $menu = $factory->createItem('root');
+        $acl  = $this->container->get('bacon_acl.service.authorization');
+
+
+
 
         $translate = $this->container->get('translator');
 
         //User
-        $menu->addChild($translate->trans('User'), ['route' => 'admin_user'])->setAttribute('icon', '<i class="fa fa-user"></i>');
-
+        if ($acl->authorize('users', 'INDEX')) {
+            $menu->addChild($translate->trans('User'), ['route' => 'admin_user'])->setAttribute('icon', '<i class="fa fa-user"></i>');
+        }
         //Groups
-        $menu->addChild($translate->trans('Group of Users'), ['route' => 'fos_user_group_list'])->setAttribute('icon', '<i class="fa fa-users"></i>');
+        if ($acl->authorize('groups', 'INDEX')) {
+            $menu->addChild($translate->trans('Group of Users'), ['route' => 'fos_user_group_list'])->setAttribute('icon', '<i class="fa fa-users"></i>');
+        }
 
-        $menu->addChild($translate->trans('ACL'))->setAttribute('icon', '<i class="fa fa-lock"></i>');
-        $menu[$translate->trans('ACL')]->addChild($translate->trans('Module'), ['route' => 'module']);
-        $menu[$translate->trans('ACL')]->addChild($translate->trans('Module Actions'), ['route' => 'module_actions']);
+        if ($acl->authorize('acl', 'ACL')) {
+            $menu->addChild($translate->trans('ACL'))->setAttribute('icon', '<i class="fa fa-lock"></i>');
+        }
+
+        if ($acl->authorize('module', 'INDEX')) {
+            if (isset($menu[$translate->trans('ACL')])) {
+                $menu[$translate->trans('ACL')]->addChild($translate->trans('Module'), ['route' => 'module']);
+            }
+        }
+
+        if ($acl->authorize('module-actions', 'INDEX')) {
+            if (isset($menu[$translate->trans('ACL')])) {
+                $menu[$translate->trans('ACL')]->addChild($translate->trans('Module Actions'), ['route' => 'module_actions']);
+            }
+        }
 
         return $menu;
     }
